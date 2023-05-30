cc43f788fa3831d8aa3488584c077d7bc9a78622
illkkeerr <ilkersenel5797@gmail.com>
2023-05-30 19:52:34 +0300

    Flappy_Bird
    C# form Flappy Bird Oyunu

---
+++ Flappy_Bird/Flappy_Bird/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿using System.Windows.Forms;
+
+namespace Flappy_Bird
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private PictureBox flappyBird;
+        private PictureBox boruUst;
+        private PictureBox boruAlt;
+        private PictureBox Zemin;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.flappyBird = new System.Windows.Forms.PictureBox();
+            this.boruUst = new System.Windows.Forms.PictureBox();
+            this.boruAlt = new System.Windows.Forms.PictureBox();
+            this.Zemin = new System.Windows.Forms.PictureBox();
+            this.scoreText = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.flappyBird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boruUst)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boruAlt)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Zemin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flappyBird
+            // 
+            this.flappyBird.Image = global::Flappy_Bird.Properties.Resources.bird;
+            this.flappyBird.Location = new System.Drawing.Point(49, 204);
+            this.flappyBird.Name = "flappyBird";
+            this.flappyBird.Size = new System.Drawing.Size(81, 64);
+            this.flappyBird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.flappyBird.TabIndex = 0;
+            this.flappyBird.TabStop = false;
+            // 
+            // boruUst
+            // 
+            this.boruUst.Image = global::Flappy_Bird.Properties.Resources.pipedown;
+            this.boruUst.Location = new System.Drawing.Point(370, 2);
+            this.boruUst.Name = "boruUst";
+            this.boruUst.Size = new System.Drawing.Size(100, 156);
+            this.boruUst.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.boruUst.TabIndex = 1;
+            this.boruUst.TabStop = false;
+            // 
+            // boruAlt
+            // 
+            this.boruAlt.Image = global::Flappy_Bird.Properties.Resources.pipe;
+            this.boruAlt.Location = new System.Drawing.Point(533, 370);
+            this.boruAlt.Name = "boruAlt";
+            this.boruAlt.Size = new System.Drawing.Size(100, 172);
+            this.boruAlt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.boruAlt.TabIndex = 2;
+            this.boruAlt.TabStop = false;
+            // 
+            // Zemin
+            // 
+            this.Zemin.Image = global::Flappy_Bird.Properties.Resources.ground;
+            this.Zemin.Location = new System.Drawing.Point(-2, 535);
+            this.Zemin.Name = "Zemin";
+            this.Zemin.Size = new System.Drawing.Size(730, 50);
+            this.Zemin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Zemin.TabIndex = 3;
+            this.Zemin.TabStop = false;
+            // 
+            // scoreText
+            // 
+            this.scoreText.AutoSize = true;
+            this.scoreText.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.scoreText.Location = new System.Drawing.Point(42, 588);
+            this.scoreText.Name = "scoreText";
+            this.scoreText.Size = new System.Drawing.Size(142, 38);
+            this.scoreText.TabIndex = 4;
+            this.scoreText.Text = "score: 0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimerEvent);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(125, 321);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(386, 29);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Boşluk Tuşuna Basarak Oynayın";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Yellow;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(219, 248);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(208, 56);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "BAŞLA";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Turquoise;
+            this.ClientSize = new System.Drawing.Size(722, 632);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flappyBird);
+            this.Controls.Add(this.scoreText);
+            this.Controls.Add(this.Zemin);
+            this.Controls.Add(this.boruAlt);
+            this.Controls.Add(this.boruUst);
+            this.Name = "Form1";
+            this.Text = "Flappy Bird";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gameKeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gameKeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.flappyBird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boruUst)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boruAlt)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Zemin)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label scoreText;
+        private Timer gameTimer;
+        private Label label1;
+        private Button button1;
+    }
+}
+
---
+++ Flappy_Bird/Flappy_Bird/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flappy_Bird
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        int boruHizi = 8;
+        int gravity = 10;
+        int score = 15;
+        int scoreArtis = 0;
+        bool basla = false;
+        private void gameTimerEvent(object sender, EventArgs e)
+        {
+            if (basla == true)
+            {
+                flappyBird.Top += gravity;
+                boruAlt.Left -= boruHizi;
+                boruUst.Left -= boruHizi;
+                scoreText.Text = "Score: " + score;
+                if (boruAlt.Left < -150)
+                {
+                    boruAlt.Left = 800;
+                    score++;
+                    scoreArtis++;
+                }
+                if (boruUst.Left < -180)
+                {
+                    boruUst.Left = 950;
+                    score++;
+                    scoreArtis++;
+                }
+                if (flappyBird.Bounds.IntersectsWith(boruAlt.Bounds) || flappyBird.Bounds.IntersectsWith(boruUst.Bounds) ||
+                    flappyBird.Bounds.IntersectsWith(Zemin.Bounds))
+                {
+                    endGame();
+                }
+                if (scoreArtis > 15)
+                {
+                    boruHizi += 4;
+                    scoreArtis = 0;
+                }
+                if (flappyBird.Top < -25)
+                {
+                    endGame();
+                }
+            }
+
+        }
+
+
+        private void gameKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = -10;
+
+            }
+
+        }
+
+        private void gameKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = +10;
+            }
+        }
+        private void endGame()
+        {
+            gameTimer.Stop();
+            scoreText.Text = "Game Over!!!";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            label1.Visible = false;
+            basla = true;
+            button1.Enabled = true;
+            this.Focus();
+        }
+    }
+}
